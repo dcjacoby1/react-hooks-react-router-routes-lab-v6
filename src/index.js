a904445a14e9768b91e18a5f721c2a0476cf7972
Dan Jacoby <jacobydan01@gmail.com>
2024-04-17 08:49:08 -0400

    resubmit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 
-
+//routes in the routes.js file
 const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
